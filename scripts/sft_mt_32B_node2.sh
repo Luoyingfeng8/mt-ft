5a19a9d2d6bcf3f67b916e326b81781e13ac4b2e
Luoyingfeng8 <1650679407@qq.com>
2025-05-28 09:13:45 +0800

    commit

---
+++ scripts/sft_mt_32B_node2.sh
@@ -0,0 +1,88 @@
+#! /bin/bash
+set -eux
+ROOT_DIR=$(dirname $(dirname `readlink -f $0`))
+
+export HF_HOME="$ROOT_DIR/cache/"
+export MODELSCOPE_CACHE="$ROOT_DIR/cache/"
+export HF_EVALUATE_OFFLINE=1
+export HF_DATASETS_OFFLINE=1
+export NPROC_PER_NODE=8 
+
+# model
+model_name=Qwen3-32B
+model_dir=$ROOT_DIR/model_card/$model_name
+config_file=$ROOT_DIR/configs/ds_z3_config_bf16.json
+# resume_from_checkpoint=
+
+export NNODES=2 
+export NODE_RANK=1
+export MASTER_ADDR=127.0.0.1
+export MASTER_PORT=29500  
+
+
+# data
+max_lengths=1024
+num_train_epochs=1
+
+# sizes=(0.5 1 5 10 30 50 100 200 500)
+# batch=(8 8 8 8 8 8 8 8 8)
+# grad_accu=(1 1 2 4 8 8 16 16 24)
+
+sizes=(0.5 1 5 10 30 50 100 200)
+batch=(4 4 4 4 4 4 4 4)
+grad_accu=(1 1 2 4 8 8 16 16)
+
+for i in ${!sizes[@]}; do 
+
+    data_size=${sizes[$i]}
+    batch_size=${batch[$i]}
+    gra_ac=${grad_accu[$i]}
+
+    train_dataset=$ROOT_DIR/data/sft_train/train.${data_size}k.jsonl
+    val_dataset=$ROOT_DIR/data/sft_train/valid.jsonl
+
+    tag=sft_${data_size}k
+    output_dir=$ROOT_DIR/exps/$model_name/$tag
+    mkdir -p $output_dir
+    cp $0 $output_dir
+    
+    ####### train
+    swift sft \
+        --deepspeed  $config_file \
+        --add_version False \
+        --check_model False \
+        --model $model_dir \
+        --train_type full \
+        --attn_impl flash_attn \
+        --dataset $train_dataset \
+        --split_dataset_ratio 0 \
+        --val_dataset $val_dataset \
+        --torch_dtype bfloat16 \
+        --num_train_epochs $num_train_epochs \
+        --per_device_train_batch_size $batch_size \
+        --per_device_eval_batch_size 8 \
+        --learning_rate 8e-6 \
+        --gradient_accumulation_steps $gra_ac \
+        --save_strategy steps \
+        --logging_strategy steps \
+        --eval_strategy steps \
+        --eval_steps 0.1 \
+        --save_steps 0.1 \
+        --logging_steps 10 \
+        --max_length $max_lengths \
+        --output_dir $output_dir \
+        --create_checkpoint_symlink \
+        --warmup_ratio 0.01 \
+        --dataloader_num_workers 8 \
+        --dataset_num_proc 16 \
+        --seed 42  \
+        --report_to tensorboard \
+        --save_only_model \
+        --save_total_limit 2 \
+        --ddp_timeout 180000000 | tee $output_dir/train.log
+
+    
+    ####### predict
+    # bash inference.sh $output_dir/best
+
+done 
